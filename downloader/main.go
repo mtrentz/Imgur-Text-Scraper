a27ab8e82f2c58c9a3e67c8969a69f9c176df372
trentz <mateus.trentz@gmail.com>
2021-08-31 21:46:40 -0300

    lower num of code to test

--- downloader/main.go
+++ downloader/main.go
@@ -27,13 +27,13 @@ func main() {
 	// Amount of goroutines
 	// It can be way higher than CPU cores
 	// But sending too many requests will make your IP get locked by imgur
-	numWorkers := 10
+	numWorkers := 2
 
 	// Size of imgur code, for exemple i.imgur.com/x123xD -> 6 random characters
 	// Codes with 5 characters are older images uploaded to imgur.
 	// Codes with 6 are usually newer, but its harder to find working urls
 	// Codes with 7 are pretty new, and it can take up to minutes trying to find a working url.
-	codeLen := 7
+	codeLen := 6
 
 	counter := 0
 	urlChannel := make(chan string)
