6f233c19e0751995c80e59546deedb8fe841e4b6
trentz <mateus.trentz@gmail.com>
2021-09-07 16:08:36 -0300

    formatting only

--- analyser/database.py
+++ analyser/database.py
@@ -2,24 +2,24 @@ import sqlite3
 import os
 import sys
 
+
 def create_db():
     HERE = os.path.dirname(sys.argv[0])
 
     conn = sqlite3.connect(os.path.join(HERE, '..', 'files', 'detected_text.db'))
     c = conn.cursor()
 
-    c.execute(
-    """
+    c.execute("""
     CREATE TABLE IF NOT EXISTS texts (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         img_id TEXT,
         extension TEXT,
         text TEXT
     );
-    """
-    )
+    """)
 
     conn.commit()
+
 
 def insert_text(conn, cursor, img_id, extension, text):
     sql = "INSERT INTO texts (img_id, extension, text) VALUES (?,?,?);"
